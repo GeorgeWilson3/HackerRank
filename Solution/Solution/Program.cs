2ac888c57cb980e441c632eb99733372f272f111
gwilson <gwilson@malwarebytes.org>
2017-11-01 15:40:52 -0400

    Test

--- Solution/Solution/Program.cs
+++ Solution/Solution/Program.cs
@@ -12,7 +12,6 @@ namespace Solution
         static void Main(string[] args)
         {
             Day18();
-
         }
 
         // https://www.hackerrank.com/challenges/30-queues-stacks/problem
